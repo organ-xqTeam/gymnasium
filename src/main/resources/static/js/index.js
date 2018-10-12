b78f156f86782bfb9c9525a02eae5ed971dd873d
XingPanST <XingPanST@DESKTOP-E2I46F7>
2018-10-12 17:14:43 +0800

    修改管理页面 js css

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -23,6 +23,8 @@ var st1 = [
     , "22", "23"
 ];
 
+
+
 var Index = {
     allType: [],
 
@@ -77,7 +79,7 @@ var Index = {
     getArea: function (stid) {
         $.ajax({
             type: "post",
-            url: Global.host + "site/selectbystid",
+            url: Global.host + "site/selectbystids",
             data: {
                 stid: Number(stid)
             },
@@ -92,7 +94,11 @@ var Index = {
                     var $option1 = $('<option value="0">请选择场地</option>')
                 	$("#sid").append($option1)
                     arr.forEach(function (obj) {
-                        var $option = $('<option value="' + obj.sid + '" data-rvalue="' + obj.sname + '">' + obj.sname + '</option>')
+                        var $option = $(
+                    		'<option value="' + obj.sid + '" data-rvalue="' + obj.sname + '" data-snumber="'+obj.snumber+'">' + 
+                    			obj.sname + 
+                    		'</option>'
+                		)
                         $("#sid").append($option)
                     })
                 }
@@ -111,6 +117,29 @@ var Index = {
         var sname=$("#sid option:selected").attr("data-rvalue") //"篮球一号场地"
         var gid=$("#gid option:selected").attr("value");
         var date=$("#datetime").val()+" 00:00:00"
+        
+        
+    	let a1 = "";
+        if ("篮球" === stname) {
+    		a1 += "<div class=\"add_input court_img court_position\">"
+    			+"<div class='l' data-site='A'></div>"
+    			+"<div class='l' data-site='B'></div>"
+    			+"</div>";
+    	}else if("羽毛球" === stname){
+    		a1 += "<div class=\"add_input court_img court_position court_positionBadminton\">"
+    			+"<div class='l' data-site='C'></div>"
+    			+"<div class='l' data-site='D'></div>"
+    			+"<div class='l' data-site='E'></div>"
+    			+"<div class='l' data-site='F'></div>"
+    			+"</div>";
+    	}else{
+    		a1 += "<div class=\"add_input court_img court_positionBadminton1\">"
+    			+"<div class='l' data-site='by'></div>"
+    			+"</div>";
+    	}
+        $(".lo").html(a1);
+        
+        
         $.ajax({
             type: "post",
             url: Global.host + "orders/selectbycount",
@@ -140,32 +169,79 @@ var Index = {
             num:7
         }
         console.log(postData)
-        $("td").removeClass("select")
+        $(".l").removeClass("select")
         $.ajax({
             type: "post",
             url: Global.host + "orders/selectbyordersydhou",
             data: postData,
             success: function (data) {
             	/*alert("data"+data);*/
+            	//[{"10-12":[{"b002=2018-10-12 12:00:00":[{"sname":"北海北","snumber":"b002-A","sid":115},{"sname":"北海北","snumber":"b002-B","sid":116}]},{"b002=2018-10-12 13:00:00":[{"sname":"北海北","snumber":"b002-A","sid":115},{"sname":"北海北","snumber":"b002-B","sid":116}]},{"b002=2018-10-12 14:00:00":[{"sname":"北海北","snumber":"b002-A","sid":115},{"sname":"北海北","snumber":"b002-B","sid":116}]}]},{"10.13":[]},{"10.14":[]},{"10.15":[]},{"10.16":[]},{"10.17":[]},{"10.18":[]}]
                 console.log(data)
                 var isEmpty=true
                 data.forEach(function (obj, colindex) {
+                	console.log(obj)
                     //日期表头
-                    var date = obj.time.replace(".", "-")
+                	let date=Object.keys(obj)[0]
+                	console.log(date)
+//                    var date = obj.time.replace(".", "-")
                     $("#time th").eq(colindex + 1).html(date)
-                    //这一天的预定时间段
-                    if (obj.value.length > 0) {
-                        isEmpty=false
-                        obj.value.forEach(function (begintime) {
-                            var btime = begintime.substr(0, 5)
-                             console.log(begintime+"\t"+btime)
-                            var rowindex=bt.indexOf(btime)
-                            console.log(rowindex)
-                            
-                            if(rowindex>-1){
-                                $("#main tr").eq(rowindex).find("td").eq(colindex+1).addClass("select")
-                            }
+                    let arr =  obj[date].map(function(obj){
+                    	let date=Object.keys(obj)[0]
+                    	return date
+                    })
+                    
+//                    setTimeout(function(){
+                    	let objValueArr=obj[date] //10-12: Array(6) 数组
+                        objValueArr.forEach(function(timeObj,timeindex){
+                        	console.log(timeObj)
+                        	let timeKey=arr[timeindex] //time key "b001=2018-10-12 07:00:00"
+                        	//这个date被预定的时间段
+                        	let btime=timeKey.split("=")[1].substr(11,5)
+                        	var rowindex=bt.indexOf(btime)
+                        	console.log(rowindex,colindex)
+                        	
+                        	//找到这个td
+                        	var $td=$("#main tr").eq(rowindex).find("td").eq(colindex+1)
+                        	console.log($td)
+//                        	$td.addClass("select")
+                        	//这个时间段选中的场地数组
+                        	let selectedAreaArr=timeObj[timeKey]
+                        	console.log(selectedAreaArr)
+                        	selectedAreaArr.forEach(function(areaObj){
+                        		let site=areaObj.snumber.substr(areaObj.snumber.length-1,1)
+                        		console.log(site)
+                        		console.log($td[0])
+                        		console.log($td[0].getElementsByClassName("l")[0])
+//                        		debugger
+                        		$td.find("div.l").addClass("select")
+                        	})
                         })
+//                    },5000)
+                    
+                    
+                    
+                    console.log(arr)
+                    //这一天的预定时间段
+//                  Index.htmlLoad(date,data,stname);
+                    if (arr.length > 0) {
+                        isEmpty=false
+//                        console.log(stname) //篮球
+//                        arr.forEach(function (timeStr) {
+//                        	let btime=timeStr.split("=")[1].substr(11,5)
+//                        	var rowindex=bt.indexOf(btime)
+//                        	console.log(rowindex)
+//                            if(rowindex>-1){
+//                            	console.log($("#main tr").eq(rowindex).find("td").eq(colindex+1))
+//                            	let $td=$("#main tr").eq(rowindex).find("td").eq(colindex+1)
+//                            	if(stname){
+//                            		
+//                            	}else{
+//                            		
+//                            	}
+//                                $("#main tr").eq(rowindex).find("td").eq(colindex+1).addClass("select")
+//                            }
+//                        })
                     }
                 })
                 if(isEmpty){
@@ -193,6 +269,159 @@ var Index = {
         $("#button").click(function(){
             Index.getSevenDayData()
         })
+        
+         // 预定框点击事件
+        $("#main").undelegate();
+        $("#main").delegate(".l", "click", function () {
+            // 样式
+            if ($(this).hasClass("book_act")) {
+                $(this).removeClass('book_act').removeAttr("data-col").removeAttr("data-row")
+            } else {
+                // no_book
+                if ($(this).hasClass("select")) {
+
+                } else {
+                    $(this).addClass('book_act')
+                }
+            }
+
+            var $tr = $(this).closest("tr")
+            // 获取第几列
+// var colindex = $tr.find("td").index($(this))
+            var colindex = $tr.find("td").index($(this).closest("td"))
+             //console.log(colindex)
+            // 获取第几行
+            var rowindex = $tr.closest("tbody").find("tr").index($tr)
+            // console.log(rowindex)
+
+            // 保存 行 列
+            $(this).attr("data-col", colindex).attr("data-row", rowindex)
+        })
+        
+        
+        document.getElementById("Affirmatory").onclick=function(){
+        	Index.submit();
+        }
+        
+    },
+    submit() {
+    	let  stname =$("#stid option:selected").attr("data-rvalue") //"篮球"
+    	let  gid=$("#gid option:selected").attr("value");
+			//获取 snumber 对应 sid
+    	  $.ajax({
+              type: "get",
+              url: Global.host + "site/selectBySnumbers",
+              data: {
+            	  //?stname=%E7%AF%AE%E7%90%83&gid=50
+            	  stname:stname,
+                  gid: gid,
+              },
+              success: function (res) {
+            	  console.log(res)
+            	  let sidcount=res //数组 
+            	 //[{"sid":113,"stid":92,"srsnumber":null,"sname":"南山南","snumber":"b001-A","sprice":23.0,"pstate":1,"pstatus":1,"sremark":"","time":"2018-09-28T14:39:28.000+0000","strnumber":null,"strstname":null,"strgname":null},{"sid":114,"stid":92,"srsnumber":null,"sname":"南山南","snumber":"b001-B","sprice":23.0,"pstate":1,"pstatus":1,"sremark":"","time":"2018-09-28T14:39:28.000+0000","strnumber":null,"strstname":null,"strgname":null},{"sid":115,"stid":92,"srsnumber":null,"sname":"北海北","snumber":"b002-A","sprice":23.0,"pstate":1,"pstatus":1,"sremark":"","time":"2018-09-28T14:40:04.000+0000","strnumber":null,"strstname":null,"strgname":null},{"sid":116,"stid":92,"srsnumber":null,"sname":"北海北","snumber":"b002-B","sprice":23.0,"pstate":1,"pstatus":1,"sremark":"","time":"2018-09-28T14:40:04.000+0000","strnumber":null,"strstname":null,"strgname":null}]
+            	  let areaArr=[]
+            	  
+            	  let count=[]
+            	  let selectedSid=[] //sid的数组
+            	  let $ths=$("th[data-sid]") //表头数组
+      	  		console.log($("div.book_act[data-site]"))
+      	  		let $selectedDivs=$("div.book_act[data-site]") //所有被选中的div
+      	  		var sidStr=$("#sid option:selected").attr("data-snumber") //b002 
+      	  		$selectedDivs.each(function(index,el){ 
+      	  			let col=$(this).attr("data-col")
+      	  			console.log(col)
+//      	  			let sidStr=$ths.eq(col).attr("data-sid") //b002
+      	  			let sidStrReal=sidStr+"-"+$(this).attr("data-site") //b002-A //snumber
+      	  			console.log(sidStrReal)
+      	  			let sid=getSidBySnumber(sidStrReal,sidcount)
+      	  			console.log(sid)
+      	  			//保存自己的sid
+      	  			$(this).attr("data-mySid",sid)
+      	  			if(selectedSid.indexOf(sid)==-1){
+      	  				selectedSid.push(sid)
+      	  			}
+      	  		})
+      	  		console.log(selectedSid)
+      	  		selectedSid.forEach(function(sid){
+      	  			let obj={
+      	  				sid:sid,
+      	  				value:[]
+      	  			}
+      	  			count.push(obj)
+      	  		})
+      	  		console.log(count)
+      	  		//在循环一次所有的div
+      	  		$selectedDivs.each(function(index,el){ 
+      	  			let mysid=$(this).attr("data-mySid") //自己的sid
+      	  			//找到count里的对象的index
+      	  			let rightIndex
+      	  			count.forEach(function(obj,index2){
+      	  				if(obj.sid==mysid){
+      	  					rightIndex=index2
+      	  				}
+      	  			})
+      	  			console.log(rightIndex)
+      	  			
+      	  			//找到自己的时间段
+      	  			let row=Number($(this).attr("data-row"))
+      	  			let col=Number($(this).attr("data-col"))
+      	  			
+      	  			$("#time th").eq(col).text()
+      	  			
+      	  			console.log(row)
+      	  			let bgtime=new Date().getFullYear()+"-"+$("#time th").eq(col).text()+" "+bt[row]+":00"
+      	  			let edtime=new Date().getFullYear()+"-"+$("#time th").eq(col).text()+" "+bt[row+1]+":00"
+      	  			console.log(bgtime,edtime)
+      	  			
+      	  			//把时间段放入count里对应的对象
+      	  			count[rightIndex].value.push([bgtime,edtime])
+      	  			
+      	  		})
+      	  		
+      	  		console.log(count)
+      	  		
+      	  			let datecontent = {
+            		  gid :gid,
+            		  count:count
+            		  }
+                   console.log(datecontent)
+                   let map = datecontent
+                  $.ajax({
+                      type: "post",
+                      url: Global.host + "orders/insertorderss",
+                      //dataType : 'jsonp',\
+                      //traditional:true,
+                      data:JSON.stringify(map),
+                      contentType:"application/json; charset=utf-8",
+                      success: function (res) {
+                          console.log(res)
+                          if(res == 'login'){
+                          	alert("请先登录！");
+                          }else if(res == 'There are orders pending'){
+                          	alert("请先完成没完成的预定单再来完成！");
+                          }else if(res == '1'){
+                          	alert("羽毛球场地对应的篮球场地已经预定了!");
+                          }else if(res == 's'){
+                          	alert("抱歉，暂不可选，请选择其他场地!");
+                          }else if(res == '2'){
+                          	alert("羽毛球场地已经预定了!");
+                          }else if(res == '3'){
+                          	alert("篮球场地对应的羽毛球场地已经预定了!");
+                          }else if(res == '4'){
+                          	alert("篮球场地已经预定不可预定！");
+                          }else if(res == '99'){
+                          	alert("请选择大于当前时间的场地！");
+                          }else{
+                          	alert("恭喜您，预定成功！");
+                          }
+                          window.location.reload();
+                      }
+                  });
+            	  
+              }
+          });
+        
     },
     init: function () {
         Index.getType()
@@ -204,6 +433,7 @@ var Index = {
 }
 
 $(function () {
+	
     Index.init()
     $.ajax({
         type: "post",
@@ -292,24 +522,7 @@ $(function () {
         autoclose: true
         // locale: moment.locale('zh-cn')
     }).on('changeDate', function (ev) {
-        //alert("dsdasd"+ev);
-        /* $.ajax({
-        	type:"post",
-        	url:url+"time/calculatetime",
-        	data:{
-        		date:ev
-        	},
-        	success:function(data){
-        		var str = "<th>时间</th>";
-        		for(var i = 0; i < data.length; i++){
-        			str += "<th>"+data[i]+"</th>";
-        		}
-        		$("#time").html(str);
-        	}
-        }); */
     });
-    
-    //var date=$("#datetime").val()+" 00:00:00";
     //统计预定人数
     $.ajax({
         type: "post",
@@ -345,4 +558,13 @@ function dateToFormat(date) { //date类型
         .getMinutes());
     // s = date.getSeconds();
     return Y + M + D;
+}
+function getSidBySnumber(snumber,arr){ //str
+	let mysid
+	arr.forEach(function(obj){
+		if(obj.snumber==snumber){
+			mysid=obj.sid
+		}
+	})
+	return mysid
 }
