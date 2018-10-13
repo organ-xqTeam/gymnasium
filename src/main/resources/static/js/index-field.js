24b6d9b1c7e4a2d18c7574bfbbde8f87fd945f1d
15040 <15040@DESKTOP-FVHI4E5>
2018-10-13 17:47:42 +0800

    修改不能跨场地

--- src/main/resources/static/js/index-field.js
+++ src/main/resources/static/js/index-field.js
@@ -569,6 +569,22 @@ var Field = {
             		  gid :Field.gid,
             		  count:count
             		  }
+            	  
+            	  var dataclass = $('.book_act');
+                  let indexCol
+                  for (i = 0; i < dataclass.length; i++) {
+                 	 if (i == 0) {
+                 		 indexCol =$(dataclass[i]).attr('data-col')
+                 	 }
+                 	 if ($(dataclass[i]).attr('data-col') != indexCol) {
+         				alert("不能跨场地预定")
+         			}
+         	         //获取所有选中 的预定
+         	         //console.log($(dataclass[i]).attr('data-col') ,$(dataclass[i]).attr('data-row'))
+                 	 
+                  }
+            	  
+            	  
 //      	  			return
 //                  $("#date_content th[data-sid]").each(function(index,ele){
                   	//console.log(index)
@@ -677,54 +693,53 @@ var Field = {
                   // }
                    console.log(datecontent)
                    let map = datecontent
-                  $.ajax({
-                      type: "post",
-                      url: Global.host + "orders/insertorderss",
-                      //dataType : 'jsonp',\
-                      //traditional:true,
-                      data:JSON.stringify(map),
-                      contentType:"application/json; charset=utf-8",
-                      success: function (res) {
-                          console.log(res.count)
-                         if(res.count == 'login'){
-                          	alert("请先登录！");
-                          }else if(res.count == 'There are orders pending'){
-                          	alert("请先完成没完成的预定单再来完成！");
-                          }else if(res.count == '1'){
-                          	alert("羽毛球场地对应的篮球场地已经预定了！");
-                          }else if(res.count == 's'){
-                          	alert("抱歉，暂不可选，请选择其他场地！");
-                          }else if(res.count == '2'){
-                          	alert("羽毛球场地已经预定了!");
-                          }else if(res.count == '3'){
-                          	alert("篮球场地对应的羽毛球场地已经预定了！");
-                          }else if(res.count == '4'){
-                          	alert("篮球场地已经预定不可预定！");
-                          }else if(res.count == '99'){
-                          	alert("请选择大于当前时间的场地！");
-                          }else if(res.count == 'not.site'){
-                        	  alert("该场地没有添加场地！");
-                          }else if(res.count == "orders.not.null"){
-                        	  alert("已经预定不能预定！");
-                          }else if(res.count == "pstate"){
-                        	  alert("当前场地不可预定！");
-                          }else{
-                        	  alert("恭喜您，预定成功！");
-                          }
-                          window.location.reload();
-                      }
-                  });
+                   if (count.length == 0) {
+   					alert("请选择预定的场地")
+                    }else{
+//	                  $.ajax({
+//	                      type: "post",
+//	                      url: Global.host + "orders/insertorderss",
+//	                      //dataType : 'jsonp',\
+//	                      //traditional:true,
+//	                      data:JSON.stringify(map),
+//	                      contentType:"application/json; charset=utf-8",
+//	                      success: function (res) {
+//	                          console.log(res.count)
+//	                         if(res.count == 'login'){
+//	                          	alert("请先登录！");
+//	                          }else if(res.count == 'There are orders pending'){
+//	                          	alert("请先完成没完成的预定单再来完成！");
+//	                          }else if(res.count == '1'){
+//	                          	alert("羽毛球场地对应的篮球场地已经预定了！");
+//	                          }else if(res.count == 's'){
+//	                          	alert("抱歉，暂不可选，请选择其他场地！");
+//	                          }else if(res.count == '2'){
+//	                          	alert("羽毛球场地已经预定了!");
+//	                          }else if(res.count == '3'){
+//	                          	alert("篮球场地对应的羽毛球场地已经预定了！");
+//	                          }else if(res.count == '4'){
+//	                          	alert("篮球场地已经预定不可预定！");
+//	                          }else if(res.count == '99'){
+//	                          	alert("请选择大于当前时间的场地！");
+//	                          }else if(res.count == 'not.site'){
+//	                        	  alert("该场地没有添加场地！");
+//	                          }else if(res.count == "orders.not.null"){
+//	                        	  alert("已经预定不能预定！");
+//	                          }else if(res.count == "pstate"){
+//	                        	  alert("当前场地不可预定！");
+//	                          }else{
+//	                        	  alert("恭喜您，预定成功！");
+//	                          }
+//	                          window.location.reload();
+//	                      }
+//	                  });
+                    }
             	  
               }
           });
     	
     	
-        // var dataclass = $('.book_act');
-        // for (i = 0; i < dataclass.length; ++i) {
-        // //获取所有选中 的预定
-        // console.log($(dataclass[i]).attr('data-col') +
-		// $(dataclass[i]).attr('data-row'))
-        // }
+         
         
     },
     eventsBind() {
